591b3ba7d2071d34d59ac7d858b5abaf4051e852
Dennis The Menace <sdennisp25@hotmail.com>
2019-03-06 23:58:28 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Junction-Website/precache-manifest.a99314b72256b27df9a3118ad54d4c4e.js"
+  "/Junction-Website/precache-manifest.331a53aba03872426f4917af443a23f8.js"
 );
 
 workbox.clientsClaim();
